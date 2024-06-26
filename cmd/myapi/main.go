44243ea301585e5648433d4809ac9a7675d9f378
Sadık Sünbül <109942759+SadikSunbul@users.noreply.github.com>
2024-06-26 13:38:32 +0300

    bağlantı dosyası ayarlandı config.yaml

--- cmd/myapi/main.go
+++ cmd/myapi/main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"github.com/SadikSunbul/Onion-Architecture-With-GO/internal/application/services"
+	"github.com/SadikSunbul/Onion-Architecture-With-GO/internal/infrastructure/database"
 	"github.com/SadikSunbul/Onion-Architecture-With-GO/internal/interfaces/handlers"
 	"github.com/SadikSunbul/Onion-Architecture-With-GO/internal/interfaces/middleware"
 	"github.com/gorilla/mux"
+	"gopkg.in/yaml.v2"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
 
+type Config struct {
+	Database struct {
+		Connection string `yaml:"connection"`
+	} `yaml:"database"`
+}
+
 func main() {
+
+	configFile, err := os.ReadFile("../../config.yaml")
+	if err != nil {
+		log.Fatalf("config.yaml dosyası okunamadı: %v", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Fatalf("Yapılandırma dosyası ayrıştırılamadı: %v", err)
+	}
+
+	conn, _ := url.Parse(config.Database.Connection)
+	conn.RawQuery = "sslmode=verify-ca;sslrootcert=ca.pem"
+	database.InitDB(conn.String())
 
 	//postgresql database bağlantısını burada sağla değilse çalışmaz
 	//database.InitDB("connectionstring")
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.22.4
 require (
 	github.com/gorilla/mux v1.8.1
 	github.com/lib/pq v1.10.9
+	gopkg.in/yaml.v2 v2.4.0
 )
